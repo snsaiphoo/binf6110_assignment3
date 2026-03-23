01d49c653c8732cacdb78ebb2cdfed96b4dd3192
snsaiphoo <sabrinasaiphoo@gmail.com>
2026-03-23 01:03:17 -0400

    scripts

--- scripts/00_packages.R
+++ scripts/00_packages.R
@@ -1,5 +1,9 @@
 # Install and load required packages
 # Do this before running 10_metanalysis.R
+# Versions are
+# [1] dplyr_1.2.0       microbiome_1.32.0 ANCOMBC_2.12.1    ggplot2_4.0.2     phyloseq_1.54.2   vegan_2.7-3      
+# [7] permute_0.9-10    biomformat_1.38.3
+
 
 # Install CRAN packages
 install.packages(c("ggplot2", "dplyr", "vegan"))
